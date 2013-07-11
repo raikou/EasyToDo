75ccaa6591b3edb1b60215fdf51992d3395fc38a
rai <rai.create.program.git@gmail.com>
2013-07-12 00:34:43 +0900

    初期

---
+++ EasyToDo/Form2.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace EasyToDo
+{
+	partial class Form2
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.OK_Button = new System.Windows.Forms.Button();
+			this.Cancel_Button = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.radioButton6 = new System.Windows.Forms.RadioButton();
+			this.radioButton5 = new System.Windows.Forms.RadioButton();
+			this.radioButton4 = new System.Windows.Forms.RadioButton();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// OK_Button
+			// 
+			this.OK_Button.Location = new System.Drawing.Point(195, 237);
+			this.OK_Button.Name = "OK_Button";
+			this.OK_Button.Size = new System.Drawing.Size(75, 23);
+			this.OK_Button.TabIndex = 0;
+			this.OK_Button.Text = "OK";
+			this.OK_Button.UseVisualStyleBackColor = true;
+			this.OK_Button.Click += new System.EventHandler(this.OK_Button_Click);
+			// 
+			// Cancel_Button
+			// 
+			this.Cancel_Button.Location = new System.Drawing.Point(11, 237);
+			this.Cancel_Button.Name = "Cancel_Button";
+			this.Cancel_Button.Size = new System.Drawing.Size(75, 23);
+			this.Cancel_Button.TabIndex = 1;
+			this.Cancel_Button.Text = "キャンセル";
+			this.Cancel_Button.UseVisualStyleBackColor = true;
+			this.Cancel_Button.Click += new System.EventHandler(this.Cancel_Button_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 11);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(40, 12);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "やること";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(58, 8);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(213, 19);
+			this.textBox1.TabIndex = 3;
+			this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+			this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+			this.textBox1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.textBox1_PreviewKeyDown);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 37);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(120, 12);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "メモ  ※ <br>利用不可 ";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(11, 54);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.textBox2.Size = new System.Drawing.Size(259, 85);
+			this.textBox2.TabIndex = 5;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.radioButton6);
+			this.groupBox1.Controls.Add(this.radioButton5);
+			this.groupBox1.Controls.Add(this.radioButton4);
+			this.groupBox1.Controls.Add(this.radioButton3);
+			this.groupBox1.Controls.Add(this.radioButton2);
+			this.groupBox1.Controls.Add(this.dateTimePicker1);
+			this.groupBox1.Controls.Add(this.radioButton1);
+			this.groupBox1.Location = new System.Drawing.Point(11, 145);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(258, 86);
+			this.groupBox1.TabIndex = 6;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "期限";
+			// 
+			// radioButton6
+			// 
+			this.radioButton6.AutoSize = true;
+			this.radioButton6.Location = new System.Drawing.Point(172, 67);
+			this.radioButton6.Name = "radioButton6";
+			this.radioButton6.Size = new System.Drawing.Size(48, 16);
+			this.radioButton6.TabIndex = 6;
+			this.radioButton6.TabStop = true;
+			this.radioButton6.Text = "メモ３";
+			this.radioButton6.UseVisualStyleBackColor = true;
+			this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+			// 
+			// radioButton5
+			// 
+			this.radioButton5.AutoSize = true;
+			this.radioButton5.Location = new System.Drawing.Point(94, 67);
+			this.radioButton5.Name = "radioButton5";
+			this.radioButton5.Size = new System.Drawing.Size(48, 16);
+			this.radioButton5.TabIndex = 5;
+			this.radioButton5.TabStop = true;
+			this.radioButton5.Text = "メモ２";
+			this.radioButton5.UseVisualStyleBackColor = true;
+			this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+			// 
+			// radioButton4
+			// 
+			this.radioButton4.AutoSize = true;
+			this.radioButton4.Location = new System.Drawing.Point(17, 67);
+			this.radioButton4.Name = "radioButton4";
+			this.radioButton4.Size = new System.Drawing.Size(48, 16);
+			this.radioButton4.TabIndex = 4;
+			this.radioButton4.TabStop = true;
+			this.radioButton4.Text = "メモ１";
+			this.radioButton4.UseVisualStyleBackColor = true;
+			this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.AutoSize = true;
+			this.radioButton3.Location = new System.Drawing.Point(94, 44);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(59, 16);
+			this.radioButton3.TabIndex = 3;
+			this.radioButton3.TabStop = true;
+			this.radioButton3.Text = "無期限";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Location = new System.Drawing.Point(17, 44);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(47, 16);
+			this.radioButton2.TabIndex = 2;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "緊急";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Location = new System.Drawing.Point(94, 21);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(158, 19);
+			this.dateTimePicker1.TabIndex = 1;
+			this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+			this.dateTimePicker1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dateTimePicker1_KeyPress);
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Location = new System.Drawing.Point(17, 21);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(71, 16);
+			this.radioButton1.TabIndex = 0;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "日付指定";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+			// 
+			// Form2
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.Cancel_Button);
+			this.Controls.Add(this.OK_Button);
+			this.DataBindings.Add(new System.Windows.Forms.Binding("Location", global::EasyToDo.Properties.Settings.Default, "DialogLocation", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Location = global::EasyToDo.Properties.Settings.Default.DialogLocation;
+			this.Name = "Form2";
+			this.Text = "項目編集";
+			this.Load += new System.EventHandler(this.Form2_Load);
+			this.Shown += new System.EventHandler(this.Form2_Shown);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button OK_Button;
+		private System.Windows.Forms.Button Cancel_Button;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.RadioButton radioButton6;
+		private System.Windows.Forms.RadioButton radioButton5;
+		private System.Windows.Forms.RadioButton radioButton4;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.RadioButton radioButton1;
+	}
+}
