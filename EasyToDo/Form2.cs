62dbd9a1bea27b2e7daaa5d8f2c59e6449468db4
raidhin <raitei.zero@gmail.com>
2013-11-06 23:41:59 +0900

    新規

---
+++ EasyToDo/Form2.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EasyToDo
+{
+	public partial class Form2 : Form
+	{
+		public string date, name, memo;
+		public string check=""; // 期限を一時的に保存する物
+		public bool status = false; //true:OK false:キャンセル
+
+		public Form2()
+		{
+			InitializeComponent();
+		}
+		public Form2( string name, string memo, string date )
+		{
+			InitializeComponent();
+			this.name = name;
+			this.memo = memo;
+			this.date = date;
+
+		}
+
+		private void Form2_Load(object sender, EventArgs e)
+		{
+			try{
+				this.textBox1.Text = this.name;
+				this.textBox2.Text = this.memo;
+				if( this.date == "緊急")
+				{
+					this.radioButton2.Checked = true;
+				}
+				else if (this.date == "完了")
+				{
+					//完了の場合は表示を無期限に自動的にする
+					this.radioButton3.Checked = true;
+				}
+				else if (this.date == "無期限")
+				{
+					this.radioButton3.Checked = true;
+				}
+				else if (this.date == "メモ１")
+				{
+					this.radioButton4.Checked = true;
+				}
+				else if (this.date == "メモ２")
+				{
+					this.radioButton5.Checked = true;
+				}
+				else if (this.date == "メモ３")
+				{
+					this.radioButton6.Checked = true;
+				}
+				else
+				{
+					this.radioButton1.Checked = true;
+					this.dateTimePicker1.Text = this.date;
+				}
+			}catch(Exception ex ){
+				MessageBox.Show( ex.Message );
+			}
+		}
+
+		private void OK_Button_Click(object sender, EventArgs e)
+		{
+			this.save();
+		}
+		private void save()
+		{
+			this.status = true;
+			this.name = this.textBox1.Text;
+			this.memo = this.textBox2.Text;
+			this.date = this.check;
+			this.Close();
+		}
+
+		private void radioButton1_CheckedChanged(object sender, EventArgs e)
+		{
+			this.check = this.dateTimePicker1.Text;
+		}
+
+		private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+		{
+			this.check = this.dateTimePicker1.Text;
+			radioButton1.Checked = true;
+		}
+
+		private void radioButton2_CheckedChanged(object sender, EventArgs e)
+		{
+			this.check = "緊急";
+		}
+
+		private void radioButton3_CheckedChanged(object sender, EventArgs e)
+		{
+			this.check = "無期限";
+		}
+
+		private void radioButton4_CheckedChanged(object sender, EventArgs e)
+		{
+			this.check = "メモ１";
+		}
+
+		private void radioButton5_CheckedChanged(object sender, EventArgs e)
+		{
+			this.check = "メモ２";
+		}
+
+		private void radioButton6_CheckedChanged(object sender, EventArgs e)
+		{
+			this.check = "メモ３";
+		}
+
+		private void Cancel_Button_Click(object sender, EventArgs e)
+		{
+			//this.save();
+			this.Close();
+		}
+
+		private void dateTimePicker1_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			this.save();
+		}
+
+		private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if( e.KeyChar == (char)Keys.Return ){
+				this.save();
+			}
+		}
+
+		private void textBox1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+		{
+		}
+
+		private void textBox1_KeyDown(object sender, KeyEventArgs e)
+		{
+		}
+
+		private void Form2_Shown(object sender, EventArgs e)
+		{
+			this.textBox1.Focus();
+		}
+
+
+	}
+}
---
+++ EasyToDo/OutFileDataSet.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace EasyToDo
+{
+	public partial class OutFileDataSet
+	{
+		useXML uXml = new useXML();
+		Flags flags = new Flags();
+
+		/// <summary>
+		/// ファイル保存メッセージ＆自動実行
+		/// </summary>
+		/// <param name="file_name"></param>
+		private void SaveFile( string file_name )
+		{
+			MessageBox.Show(file_name.ToString() + "に保存しました。");
+			Process.Start( file_name.ToString() );
+
+
+		}
+
+		/// <summary>
+		/// 選択データのタイトルをファイルに出力する。
+		/// </summary>
+		/// <param name="data">項目データ</param>
+		public void AllDataTitle( Datas data )
+		{
+			SaveFileDialog sfd = new SaveFileDialog();
+
+			flags = uXml.read();
+
+			sfd.FileName = "全データタイトル一覧.txt";
+			if( sfd.ShowDialog() == DialogResult.OK ){
+
+				StreamWriter ofs = new StreamWriter( sfd.FileName );
+				for( int i=0; i<data.data.Count; i++){
+
+					//フィルターに引っかかる物は表示しない
+					if (uXml.check(data.data[i].now_status, flags) != true)
+					{
+						continue;
+					}
+
+					//ofs.WriteLine("{0}\t:{1}", data.data[i].getStatus().ToString(), data.data[i].name.ToString());
+					ofs.WriteLine("{1}（期限：{0}）", data.data[i].getStatusTextForDisp().ToString(), data.data[i].name.ToString());
+				}
+				ofs.Close();
+				
+				//MessageBox.Show(sfd.FileName.ToString()+"に保存しました。");
+				SaveFile(sfd.FileName.ToString());
+
+			}
+
+		}
+
+		/// <summary>
+		/// 選択データのタイトル及び内容を出力する。
+		/// タイトルのみは AllDataTitle を利用する。
+		/// </summary>
+		/// <param name="data">項目データ</param>
+		public void AllDatas( Datas data )
+		{
+			SaveFileDialog sfd = new SaveFileDialog();
+
+			flags = uXml.read();
+
+
+			sfd.FileName = "全データ一覧.txt";
+			if (sfd.ShowDialog() == DialogResult.OK)
+			{
+
+				StreamWriter ofs = new StreamWriter(sfd.FileName);
+				for (int i = 0; i < data.data.Count; i++)
+				{
+					//フィルターに引っかかる物は表示しない
+					if (uXml.check(data.data[i].now_status, flags) != true)
+					{
+						continue;
+					}
+
+					//ofs.WriteLine("{0}\t:{1}", data.data[i].getStatus().ToString(), data.data[i].name.ToString());
+					ofs.Write("【{0}】【{1}】", data.data[i].getStatusTextForDisp().ToString(), data.data[i].name.ToString());
+					ofs.Write(Environment.NewLine);
+					ofs.Write("（作成日：{0}）", data.data[i].createDate.ToString());
+					ofs.Write("（期限：{0}）", data.data[i].limitDate.ToString());
+					ofs.Write("（終了日：{0}）", data.data[i].exitDate.ToString());
+					ofs.Write(Environment.NewLine);
+					ofs.Write("【メモ】" + Environment.NewLine);
+
+					ofs.Write("\t{0}", data.data[i].memo.Replace(Environment.NewLine, Environment.NewLine + "\t").ToString());
+					ofs.Write(Environment.NewLine);
+					ofs.Write(Environment.NewLine);
+				}
+				ofs.Close();
+				//MessageBox.Show(sfd.FileName.ToString() + "に保存しました。");
+				SaveFile(sfd.FileName.ToString());
+
+			}
+		}
+	}
+}
